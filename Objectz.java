b2835aac296699e897c606c66b3c4a13cc8df42c
Cikojack99 <cikojack99@gmail.com>
2014-10-27 03:15:00 +0700

    Objectz
    superclass objek" dalam game

---
+++ Objectz.java
@@ -0,0 +1,65 @@
+
+import java.awt.Point;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * class untuk objek yang dapat diambil
+ * @author User
+ */
+public abstract class Objectz {
+    /**
+     *type adalah penanda dalam bentuk string jenis objek ini
+     */
+    private final String type;
+    
+    /**
+     * position adalah koordinat objek ini
+     */
+    private Point position;
+
+    /**
+     * konstraktor kelas objek
+     * @param type string untuk jenis objek
+     * @param position koordinat objek dalam bentuk Point(x, y) 
+    */
+    public Objectz(String type, Point position) {
+        this.type = type;
+        this.position = position;
+    }
+    
+    /**
+     * effect dari objek ini
+     */
+    public abstract void effect();
+    
+    /**
+     * mengembalikan jenis objek ini
+     * @return jenis objek ini
+     */
+    public String getType()
+    {
+        return type;
+    }
+    
+    /**
+     * mengembalikan koordinat objek
+     * @return koordinat objek
+     */
+    public Point getCoordinate()
+    {
+        return position;
+    }
+    
+    /**
+     * memindahkan koordinat objek setelah diambil
+     */
+    public void deleteCoordinate()
+    {
+        position= new Point(0, 0);
+    }
+}
