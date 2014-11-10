edbc3173dac73d93d4f571a79971f54ff9dcf99e
GGSunshiners <harseto.pandityo@gmail.com>
2014-11-10 09:01:00 +0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,5 @@
 /*
- * Class untuk mengandalikan movement player dan pergerakan player.
+ * Class untuk mengandalikan movement player (Dev ALPHA NOTE: Mungkin inventory juga)
  */
 
 package Chip.Component;
@@ -11,15 +11,34 @@ import java.awt.Point;
  * @version 0.01 ALPHA
  */
 public class Player {
-    
+   
+    /**
+     * Sebuah kordinat posisi player pada saat ini.
+     */
     private Point currentPosition;
+    
+    /**
+     * (Dev ALPHA NOTE: Inventory mungkin diimplementasikan di class player)
+     */
     private Item[] inventory;
     
+    /**
+     * Constructur ini berfungsi untuk menginisialisasikan posisi kordinat awal 
+     * player dalam sebuah level.
+     * @param startingPosition Posisi awal player.
+     */
     public Player(Point startingPosition)
     {
         currentPosition=startingPosition;
     }
     
+    /**
+     * Method ini berfungsi untuk memindahkan kordinat player berdasarkan arah
+     * yang user arahkan, input arah akan diurus oleh class Board.
+     * @param direction arah input player yang sudah dikonversi sebagai int.
+     * 0=Up, 1=Down, 2=left, 3=right.
+     * @return Point posisi player yang sudah bergerak.
+     */
     public Point move(int direction)
     {
         if(direction==0)
@@ -41,6 +60,11 @@ public class Player {
         return currentPosition;
     }
     
+    /**
+     * Method ini berfungsi untuk memanggil posisi Point player pada saat ini
+     * untuk digunakan pada class lain.
+     * @return posisi Point player pada saat ini.
+     */
     public Point getCurPosition()
     {
         return currentPosition;
