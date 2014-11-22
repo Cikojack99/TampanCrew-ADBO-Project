b68c6da8defb17b0faa4f0e5fcc3806dd8230468
GGSunshiners <harseto.pandityo@gmail.com>
2014-11-22 12:31:20 +0700

    Nambahin in obstacle sama in items.

--- Component/Levels/LevelZero.java
+++ Component/Levels/LevelZero.java
@@ -106,17 +106,54 @@ public class LevelZero extends Level {
 
     @Override
     public void initializeObstacles() {
-        
+        obstacles[0]=new BrownLockedDoor();
+        obstacles[1]=new SilverLockedDoor();
+        obstacles[2]=new GreenLockedDoor();
+        obstacles[3]=new Laser();
+        obstacles[4]=new SleepingGuardRadius();
+        obstacles[5]=new FinishLineDoor();
+        Maps laser=new Maps("obstacle","laser");
+        Maps sleepingGuardRadius=new Maps("obstacle","sleepingGuardRadius");
+        petaLevel[6][13]=new Maps("obstacle","silverDoor");
+        petaLevel[11][14]=laser;
+        petaLevel[11][15]=laser;
+        petaLevel[11][16]=laser;
+        petaLevel[11][17]=laser;
+        petaLevel[21][15]=new Maps("obstacle","greenDoor");
+        petaLevel[13][13]=new Maps("obstacle","brownDoor");
+        petaLevel[12][17]=laser;
+        petaLevel[13][17]=laser;
+        petaLevel[14][17]=laser;
+        petaLevel[15][17]=laser;
+        petaLevel[22][10]=laser;
+        petaLevel[22][11]=laser;
+        petaLevel[22][12]=laser;
+        petaLevel[18][6]=sleepingGuardRadius;
+        petaLevel[18][7]=sleepingGuardRadius;
+        petaLevel[18][8]=sleepingGuardRadius;
+        petaLevel[17][7]=sleepingGuardRadius;
+        petaLevel[17][8]=sleepingGuardRadius;
+        petaLevel[16][6]=sleepingGuardRadius;
+        petaLevel[16][7]=sleepingGuardRadius;
+        petaLevel[16][8]=sleepingGuardRadius;
+        petaLevel[17][6]=new Maps("prop","sleepingGuardRadius");
+        petaLevel[25][3]=laser;
+        petaLevel[29][11]=new Maps("obstacle","FinishLineDoor");
     }
 
     @Override
     public void initializeItems() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void initializeMaps() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Maps diamond=new Maps("item","diamond");
+        petaLevel[10][6]=new Maps("item","silverKey");
+        petaLevel[10][9]=diamond;
+        petaLevel[10][14]=new Maps("item","greenKey");
+        petaLevel[7][18]=diamond;
+        petaLevel[2][19]=new Maps("item","mirrorArmor");
+        petaLevel[17][17]=new Maps("item","brownKey");
+        petaLevel[14][17]=diamond;
+        petaLevel[12][12]=new Maps("item","silentBoots");
+        petaLevel[13][7]=diamond;
+        petaLevel[26][9]=diamond;
     }
 
     @Override
