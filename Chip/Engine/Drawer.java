344ecac3205ad14e83efd371ee77981dedf4ea36
Cikojack99 <VinsVin88>
2014-11-22 14:06:11 +0700

    update

--- Chip/Engine/Drawer.java
+++ Chip/Engine/Drawer.java
@@ -7,9 +7,14 @@
 package Chip.Engine;
 
 import Chip.Component.Levels.Level;
+import Chip.Component.Levels.LevelZero;
 import Chip.Component.Maps;
 import java.awt.*;
 import java.awt.geom.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -33,13 +38,35 @@ public class Drawer extends JPanel{
         gd.setStroke(s);
         Line2D line;
         gd.setColor(Color.GRAY);
-        stage.initializeWalls();
+        stage.initializeLevel();
         Maps[][] map = stage.getMaps();
-        for (int i = 0; i < stage.getWidth(); i++) {
-            for (int j = 0; j < stage.getHeight(); j++) {
-                if(map[i][j]!=null && map[i][j].getType().contentEquals("Wall"))
+        URL imgUrl = getClass().getClassLoader().getResource("tegel.jpg");
+        Image img = null;
+        try {
+            img = ImageIO.read(imgUrl);
+        } catch (IOException ex) {
+            Logger.getLogger(Drawer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        for (int i = 0; i < 601; i+=100) {
+            for (int j = 0; j < 440; j+=100) {
+                g.drawImage(img, i, j, 100, 100, null);
+            }
+        }
+        
+        
+        imgUrl = getClass().getClassLoader().getResource("chip.jpg");
+        img = null;
+        try {
+            img = ImageIO.read(imgUrl);
+        } catch (IOException ex) {
+            Logger.getLogger(Drawer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 22; j++) {
+                if(map[i][j]!=null && map[i][j].getType().equalsIgnoreCase("Wall"))
                 {
-                    g.drawLine(i*20, j*20, i*20, j*20);
+                    g.drawImage(img, i*20, j*20, 20, 20, null);
                 }
                 
                 if(i==21 && j==29) {i=Integer.MAX_VALUE;}
